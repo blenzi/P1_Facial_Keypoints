fc1dc7f2dac73f52a2d0174e32cddb4e1b214e15
Bruno Lenzi <Bruno.Lenzi@cern.ch>
2020-04-20 20:16:58 +0200

    Arch: conv1, batchNorm1, conv2, batchNorm2, MaxPool, fc, tanh

--- models.py
+++ models.py
@@ -20,18 +20,35 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 5, padding=1)
+        self.bn1 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True)
+        
+        self.conv2 = nn.Conv2d(32, 32, 5)
+        self.bn2 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32*109*109, 68)
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
+        x = self.bn1(F.relu(self.conv1(x)))
+        x = self.bn2(F.relu(self.conv2(x)))
+        x = self.pool(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
+
+        # prep for linear layer
+        # flatten the inputs into a vector
+        x = x.view(x.size(0), -1)
+        
+        # two linear layers
+        x = self.fc1(x)
+        
+        # transform to range [-1,1]
+        x = F.tanh(x)
+
         return x
