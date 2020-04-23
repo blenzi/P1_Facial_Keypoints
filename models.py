de48a20d8b89dee0aa2a9573becd87e8357b2630
Bruno Lenzi <Bruno.Lenzi@cern.ch>
2020-04-23 10:50:55 +0200

    4 conv 3x3 with stride 2, 2 FCs 256

--- models.py
+++ models.py
@@ -20,24 +20,44 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=2)
         self.bn1 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True)
         
-        self.conv2 = nn.Conv2d(32, 32, 5)
-        self.bn2 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2)
+        self.bn2 = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True)
+
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2)
+        self.bn3 = nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True)
         
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=2)
+        self.bn4 = nn.BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True)
+                
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32*109*109, 68*2)
+#        self.pool = nn.MaxPool2d(2, 2)
+#        self.fc1 = nn.Linear(128*28*28, 68*2)
+#        self.pool = nn.MaxPool2d(5, 3)
+#        self.fc1 = nn.Linear(128*13*13, 68*2)
+#        self.fc1 = nn.Linear(128*13*13, 512)
+#        self.fc2 = nn.Linear(512, 68*2)
+#        self.fc1 = nn.Linear(256*6*6, 68*2)
+        self.fc1 = nn.Linear(256*13*13, 256)
+        #self.fc1 = nn.Linear(256*6*6, 256)
+        self.fc2 = nn.Linear(256, 68*2)
+
+
+
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        x = self.bn1(F.relu(self.conv1(x)))
-        x = self.bn2(F.relu(self.conv2(x)))
-        x = self.pool(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        #x = self.pool(x)
+        #x = self.pool(self.bn4(F.relu(self.conv4(x))))
         
         # a modified x, having gone through all the layers of your model, should be returned
 
@@ -45,10 +65,12 @@ class Net(nn.Module):
         # flatten the inputs into a vector
         x = x.view(x.size(0), -1)
         
-        # two linear layers
-        x = self.fc1(x)
+        # linear layers
+        #x = self.fc1(x)
+        x = torch.tanh(self.fc1(x))
+        x = self.fc2(x)
         
-        # transform to range [-1,1]
-        x = F.tanh(x)
+        # limit range to [-1,1]
+#        x = F.tanh(x)
 
         return x
